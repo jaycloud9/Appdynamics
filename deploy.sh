63cfb033290a345a64e7e44b86bf1e2bdf1aa416
Matt Smith <soimafreak@gmail.com>
2016-11-04 15:08:10 +0000

    Updated deploy to take variable

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID='AKIAJBA2IIPAYO3ESTKA'
-export AWS_SECRET_ACCESS_KEY='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'
 
-terraform apply ./plans/
-ansible-playbook playbook.yml -u ec2-user --private-key=keys/key.pem
+if [[ $1 != "" ]]; then
+  provider=$1
+  user="ec2-user"
+  shift
+
+  if [[ $provider == 'aws' || $provider == 'azure' ]]; then
+    echo "Executing against provider: $provider"
+    export AWS_ACCESS_KEY_ID='AKIAJBA2IIPAYO3ESTKA'
+    export AWS_SECRET_ACCESS_KEY='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'
+    terraform apply ./plans/$provider
+    ansible-playbook playbook.yml -u $user --private-key=keys/key.pem
+  else
+    echo "Please specify a valid provider: aws | azure"
+    exit 1
+  fi
+else
+  echo "Please specify an infrastructure provider. Valid options are: aws | azure"
+  exit 1
+fi
