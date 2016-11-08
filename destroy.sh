aba1aebf83651f6e4a59df0aa57cb8b7669762ab
Matt Smith <soimafreak@gmail.com>
2016-11-08 11:05:31 +0000

    Added pre-shutdown of instances to help with TF issue

--- destroy.sh
+++ destroy.sh
@@ -17,6 +17,8 @@ else
   rc=$?
   if [[ $rc == "0" ]]; then
     echo "Success"
+    echo "Stopping Instances prior to termination"
+    ansible all -u ec2-user --private-key=keys/key.pem -m shell -a "shutdown -h now"
     terraform destroy ./plans/
   else
     echo "Failed to unsubscribe all hosts or they are already unsubscribed."
