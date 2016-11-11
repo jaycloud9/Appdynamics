57f4c29f0bb98a3af4f0f5f7e621e146f3e5f107
Matt Smith <soimafreak@gmail.com>
2016-11-11 10:56:34 +0000

    Added Environment variable

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,17 @@ if [[ $1 != "" ]]; then
 
   if [[ $provider == 'aws' || $provider == 'azure' ]]; then
     echo "Executing against provider: $provider"
-    export AWS_ACCESS_KEY_ID='AKIAJBA2IIPAYO3ESTKA'
-    export AWS_SECRET_ACCESS_KEY='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'
-    terraform apply ./plans/$provider
-    ansible-playbook playbook.yml -u $user --private-key=keys/key.pem
+    if [[ $1 != "" ]]; then
+      env=$1
+      shift
+      export AWS_ACCESS_KEY_ID='AKIAJBA2IIPAYO3ESTKA'
+      export AWS_SECRET_ACCESS_KEY='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'
+      terraform apply ./plans/$provider
+      ansible-playbook playbook.yml -u $user --private-key=keys/key.pem
+    else
+      echo "You must specify an environment"
+      exit 1
+    fi
   else
     echo "Please specify a valid provider: aws | azure"
     exit 1
