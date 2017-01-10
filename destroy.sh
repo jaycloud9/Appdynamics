50e0a5a5128d42465c89fee01bdaae24fc5e4f6a
Matt Smith <soimafreak@gmail.com>
2017-01-10 09:57:24 +0000

    Removed env variable

--- destroy.sh
+++ destroy.sh
@@ -6,42 +6,35 @@ if [[ $1 != "" ]]; then
 
   if [[ $provider == 'aws' || $provider == 'azure' ]]; then
     echo "Executing against provider: $provider"
-    if [[ $1 != "" ]]; then
-      env=$1
-      shift
-      if [[ $provider == "aws" ]]; then
-        export AWS_ACCESS_KEY_ID='AKIAJBA2IIPAYO3ESTKA'
-        export AWS_SECRET_ACCESS_KEY='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'
-        user="ec2-user"
-        inventory= "inventory/ec2.py"
-      else
-        user="mpadmin"
-        inventory="inventory/azure_rm.py"
-      fi
-			if [[ $1 == "--force" ]]; then
-				echo "Forcing remove of servers with out attempting to unsubscribe"
-				echo "Are you sure you wish to continue? Type 'yes' to continue"
-				read ans
-				if [ $ans == 'yes' ]; then
-          python oscp_infra.py destroy cluster1
-				fi
-			else
-				echo "Removing subscriptions from ALL HOSTS"
-				ansible -i $inventory all -u $user --private-key=keys/key.pem -m shell -a "subscription-manager remove --all; subscription-manager unregister"
-				rc=$?
-				if [[ $rc == "0" ]]; then
-					echo "Success"
-					echo "Stopping Instances prior to termination"
-					ansible -i $inventory all -u $user --private-key=keys/key.pem -m shell -a "shutdown -h now"
-          python oscp_infra.py destroy cluster1
-				else
-					echo "Failed to unsubscribe all hosts or they are already unsubscribed."
-					echo "Ensure all systems unsubscribed and then add the --force option"
-				fi
-			fi
+    if [[ $provider == "aws" ]]; then
+      export AWS_ACCESS_KEY_ID='AKIAJBA2IIPAYO3ESTKA'
+      export AWS_SECRET_ACCESS_KEY='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'
+      user="ec2-user"
+      inventory= "inventory/ec2.py"
     else
-      echo "You must specify an environment"
-      exit 1
+      user="mpadmin"
+      inventory="inventory/azure_rm.py"
+    fi
+    if [[ $1 == "--force" ]]; then
+      echo "Forcing remove of servers with out attempting to unsubscribe"
+      echo "Are you sure you wish to continue? Type 'yes' to continue"
+      read ans
+      if [ $ans == 'yes' ]; then
+        python oscp_infra.py destroy cluster1
+      fi
+    else
+      echo "Removing subscriptions from ALL HOSTS"
+      ansible -i $inventory all -u $user --private-key=keys/key.pem -m shell -a "subscription-manager remove --all; subscription-manager unregister"
+      rc=$?
+      if [[ $rc == "0" ]]; then
+        echo "Success"
+        echo "Stopping Instances prior to termination"
+        ansible -i $inventory all -u $user --private-key=keys/key.pem -m shell -a "shutdown -h now"
+        python oscp_infra.py destroy cluster1
+      else
+        echo "Failed to unsubscribe all hosts or they are already unsubscribed."
+        echo "Ensure all systems unsubscribed and then add the --force option"
+      fi
 		fi
   else
     echo "Please specify a valid provider: aws | azure"
