b5818eecd5f8b53816dce4d4e61a4dd02a200015
Matt Smith <soimafreak@gmail.com>
2017-03-20 16:48:58 +0000

    Added dependencies for Networking

--- PlatformInfra/setup.py
+++ PlatformInfra/setup.py
@@ -5,8 +5,15 @@ from setuptools import setup
 setup(
     name='platforminfra',
     packages=['platforminfra'],
+    version='0.0.2',
     include_package_data=True,
     install_requires=[
         'flask',
+        'azure-common',
+        'msrest',
+        'msrestazure',
+        'azure-mgmt-storage',
+        'azure-mgmt-resource',
+        'azure-mgmt-network'
     ],
 )
