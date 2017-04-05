e59a2606bff92f8264673b29af374b9b7966c7c3
Matt Smith <soimafreak@gmail.com>
2017-04-05 20:23:18 +0100

    Hard set lib versions

--- PlatformInfra/setup.py
+++ PlatformInfra/setup.py
@@ -12,19 +12,19 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
-        'azure-common',
-        'msrest',
-        'msrestazure',
-        'azure-mgmt-storage',
-        'azure-storage',
-        'azure-mgmt-resource',
-        'azure-mgmt-network',
-        'azure-mgmt-compute',
-        'azure-mgmt-dns',
+        'azure-common==1.1.4',
+        'msrest==0.4.6',
+        'msrestazure==0.4.7',
+        'azure-mgmt-storage==0.31.0',
+        'azure-storage==0.34.0',
+        'azure-mgmt-resource==0.31.0',
+        'azure-mgmt-network==0.30.0',
+        'azure-mgmt-compute==0.33.0',
+        'azure-mgmt-dns==1.0.0',
         'python-jenkins',
         'python-gitlab',
-        'pyyaml'
-
+        'pyyaml',
+        'uwsgi'
     ],
     tests_require=[
         'dnspython'
