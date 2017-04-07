31a5d07f305be21cbe013e8367634a0fdacda59e
Shobin Sathyan <shobin.sathyan@gmail.com>
2017-04-07 15:05:22 +0100

    Addiing pyenv path

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,9 +2,9 @@
 
 cd /root/
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-export PATH="/root/.pyenv/bin:$PATH" >> /etc/profile
+export PATH="/root/.pyenv/bin:/root/.pyenv/shims/:$PATH"
 cd /opt/PlatformInfra
-export PATH="/root/.pyenv/bin:$PATH"; eval "$(pyenv virtualenv-init -)"
+eval "$(pyenv virtualenv-init -)"
 pyenv install 3.5.1
 pyenv shell 3.5.1
-make setup
+make setupp
